c44c4765f3c0a124a3a5a4533d7a48562557d7a8
enfinity <festusowumi@gmail.com>
2024-01-24 07:48:52 +0100

    updated repository for bc gb internal sandbox data

--- bridgecard_core_sdk/core/core_db/repository/bc_gb_internal_sandbox_repository.py
+++ bridgecard_core_sdk/core/core_db/repository/bc_gb_internal_sandbox_repository.py
@@ -9,24 +9,28 @@ BC_GB_INTERNAL_SANDBOX_DATA_MODEL_NAME = "bc_gb_internal_sandbox_data"
 
 ACCOUNTS_MODEL_NAME = "accounts"
 
+
 class BcGbInternalSandboxRepository(BaseRepository):
-    
     def __init__(
         self, db_session_factory: Callable[..., AbstractContextManager[DbSession]]
     ):
         with db_session_factory() as db_session:
-
-            db_ref = db.reference(BC_GB_INTERNAL_SANDBOX_DATA_MODEL_NAME, db_session.accounts_db_app)
+            db_ref = db.reference(
+                BC_GB_INTERNAL_SANDBOX_DATA_MODEL_NAME, db_session.accounts_db_app
+            )
 
             self.db_ref = db_ref
 
-    def create_raw(self, id: str, schema_dict, context):
+    def create_raw(
+        self,
+        id: str,
+        schema_dict,
+        context: Optional[Any] = None,
+    ):
         try:
-
             data = self.db_ref.child(ACCOUNTS_MODEL_NAME).child(id).get()
 
             if data is not None:
-
                 return False
 
             self.db_ref.child(ACCOUNTS_MODEL_NAME).child(id).set(schema_dict)
@@ -34,10 +38,13 @@ class BcGbInternalSandboxRepository(BaseRepository):
             return True
 
         except:
-            
             return False
-        
-    def read(self, id: str, context):
+
+    def read(
+        self,
+        id: str,
+        context: Optional[Any] = None,
+    ):
         try:
             data = self.db_ref.child(ACCOUNTS_MODEL_NAME).child(id).get()
 
@@ -45,8 +52,27 @@ class BcGbInternalSandboxRepository(BaseRepository):
 
         except:
             return False
-        
-    def read_attr(self, id: str, field: str, context):
+
+    def read_attr(
+        self,
+        id: str,
+        field: str,
+        context: Optional[Any] = None,
+    ):
+        try:
+            data = self.db_ref.child(ACCOUNTS_MODEL_NAME).child(id).child(field).get()
+
+            return data
+
+        except:
+            return False
+
+    def update_attr(
+        self,
+        id: str,
+        field: str,
+        context: Optional[Any] = None,
+    ):
         try:
             data = self.db_ref.child(ACCOUNTS_MODEL_NAME).child(id).child(field).get()
 
