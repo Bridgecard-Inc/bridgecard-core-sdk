fee379f78d7927332852be836a4d4b4436e3050a
enfinity <festusowumi@gmail.com>
2024-01-25 00:57:07 +0100

    updated repository for accounts db

--- bridgecard_core_sdk/core/core_db/repository/accounts_repository.py
+++ bridgecard_core_sdk/core/core_db/repository/accounts_repository.py
@@ -14,9 +14,7 @@ class AccountsRepository(BaseRepository):
         self, db_session_factory: Callable[..., AbstractContextManager[DbSession]]
     ):
         with db_session_factory() as db_session:
-            db_ref = db.reference(
-                ACCOUNTS_MODEL_NAME, db_session.accounts_db_app
-            )
+            db_ref = db.reference(ACCOUNTS_MODEL_NAME, db_session.accounts_db_app)
 
             self.db_ref = db_ref
 
@@ -28,15 +26,18 @@ class AccountsRepository(BaseRepository):
         context: Optional[Any] = None,
     ):
         try:
+            data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .child(account_id)
+                .get()
+            )
 
-            data = self.db_ref.child(company_issuing_app_id).child(environment.value).child(account_id).get()
-                
             return True
 
         except:
-            
             return None
-        
+
     def set_account_data(
         self,
         environment: EnvironmentEnum,
@@ -46,36 +47,17 @@ class AccountsRepository(BaseRepository):
         context: Optional[Any] = None,
     ):
         try:
+            data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .child(account_id)
+                .set(value)
+            )
 
-            data = self.db_ref.child(company_issuing_app_id).child(environment.value).child(account_id).set(value)
-                
             return True
 
         except:
-            
             return None
-        
-
-    def set_account_data_as_a_transaction(
-        self,
-        environment: EnvironmentEnum,
-        company_issuing_app_id: str,
-        account_id: str,
-        value,
-        context: Optional[Any] = None,
-    ):
-        try:
-
-            data = self.db_ref.child(company_issuing_app_id).child(environment.value).child(account_id).transaction(
-                    lambda current_value: (current_value or 0) + int(value)
-                )
-                
-            return True
-
-        except:
-            
-            return None
-
 
     def upddate_account_data(
         self,
@@ -86,13 +68,16 @@ class AccountsRepository(BaseRepository):
         context: Optional[Any] = None,
     ):
         try:
+            data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .child(account_id)
+                .update(value)
+            )
 
-            data = self.db_ref.child(company_issuing_app_id).child(environment.value).child(account_id).update(value)
-                
             return True
 
         except:
-            
             return None
 
     def fetch_account_data_attr(
@@ -104,9 +89,14 @@ class AccountsRepository(BaseRepository):
         context: Optional[Any] = None,
     ):
         try:
+            data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .child(account_id)
+                .child(attribute)
+                .get()
+            )
 
-            data = self.db_ref.child(company_issuing_app_id).child(environment.value).child(account_id).child(attribute).get()
-                
             return data
 
         except:
@@ -122,33 +112,63 @@ class AccountsRepository(BaseRepository):
         context: Optional[Any] = None,
     ):
         try:
+            data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .child(account_id)
+                .child(child_atrr)
+                .set(value)
+            )
 
-            data = self.db_ref.child(company_issuing_app_id).child(environment.value).child(account_id).child(child_atrr).set(value)
-                
             return True
 
         except:
-            
             return None
 
-    
-    def delete_account(self,
+    def set_account_child_atrr_data_as_a_transaction(
+        self,
         environment: EnvironmentEnum,
         company_issuing_app_id: str,
         account_id: str,
-        value:str,
+        child_atrr: str,
+        value,
         context: Optional[Any] = None,
     ):
         try:
+            
+            data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .child(account_id)
+                .child(child_atrr)
+                .transaction(lambda current_value: (current_value or 0) + int(value))
+            )
 
-            data = self.db_ref.child(company_issuing_app_id).child(environment.value).child(account_id).delete()
-                
             return True
 
         except:
-            
             return None
 
+    def delete_account(
+        self,
+        environment: EnvironmentEnum,
+        company_issuing_app_id: str,
+        account_id: str,
+        value: str,
+        context: Optional[Any] = None,
+    ):
+        try:
+            data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .child(account_id)
+                .delete()
+            )
+
+            return True
+
+        except:
+            return None
 
     def delete_account_data_attr(
         self,
@@ -159,35 +179,42 @@ class AccountsRepository(BaseRepository):
         context: Optional[Any] = None,
     ):
         try:
+            data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .child(account_id)
+                .child(attribute)
+                .delete()
+            )
 
-            data = self.db_ref.child(company_issuing_app_id).child(environment.value).child(account_id).child(attribute).delete()
-                
             return True
 
         except:
             return None
-
 
     def accounts_filter_db(
         self,
         environment: EnvironmentEnum,
         company_issuing_app_id: str,
         child_atrr: str,
-        value:str,
+        value: str,
         context: Optional[Any] = None,
     ):
         try:
+            ordered_dict_data = (
+                self.db_ref.child(company_issuing_app_id)
+                .child(environment.value)
+                .order_by_child(child_atrr)
+                .equal_to(value)
+                .get()
+            )
 
-            ordered_dict_data = self.db_ref.child(company_issuing_app_id).child(environment.value).order_by_child(child_atrr).equal_to(value).get()
-                
             dict_data = dict(ordered_dict_data)
 
             if ordered_dict_data is None:
-
                 return None
 
             elif dict_data == {}:
-
                 return None
 
             dict_key = list(dict_data.keys())[0]
@@ -195,5 +222,4 @@ class AccountsRepository(BaseRepository):
             return dict_data[dict_key]
 
         except:
-            
             return None
