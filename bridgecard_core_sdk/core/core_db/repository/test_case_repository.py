2866dfe9aaf6760622fa66625e81bbfd0df716f3
Ife-the-lion <olorunfemi_i@yahoo.com>
2024-03-03 20:23:19 +0100

    filter test db

--- bridgecard_core_sdk/core/core_db/repository/test_case_repository.py
+++ bridgecard_core_sdk/core/core_db/repository/test_case_repository.py
@@ -52,3 +52,30 @@ class TestServiceRepo(BaseRepository):
         except:
 
             return None
+
+    def test_group_filter_db(
+        self,
+        tag: str,
+        child_atrr: str,
+        value,
+        context: Optional[Any] = None,
+    ):
+        try:
+            ordered_dict_data = (
+                self.db_ref.child(tag).order_by_child(child_atrr).equal_to(value).get()
+            )
+
+            dict_data = dict(ordered_dict_data)
+
+            if ordered_dict_data is None:
+                return None
+
+            elif dict_data == {}:
+                return None
+
+            dict_key = list(dict_data.keys())[0]
+
+            return dict_data[dict_key]
+
+        except:
+            return None
