c080d647aa585e6147dbecccd99e1eec891c49c1
enfinity <festusowumi@gmail.com>
2024-03-05 18:44:08 +0100

    updated transaction monitoring service code

--- bridgecard_core_sdk/issuing/transaction_monitoring_service/protos/transaction_monitoring_details_pb2_grpc.py
+++ bridgecard_core_sdk/issuing/transaction_monitoring_service/protos/transaction_monitoring_details_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import transaction_monitoring_details_sdk_pb2 as transaction__monitoring__details__sdk__pb2
+from . import transaction_monitoring_details_sdk_pb2 as transaction__monitoring__details__sdk__pb2
 
 
 class TransactionMonitoringServiceStub(object):
