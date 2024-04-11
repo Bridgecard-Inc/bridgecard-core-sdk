fd9dc6279b405cda21cbb4dd9867966879365e4a
Ife-the-lion <olorunfemi_i@yahoo.com>
2024-04-11 08:43:16 +0200

    update admin

--- bridgecard_core_sdk/core/core_db/repository/admin_repository.py
+++ bridgecard_core_sdk/core/core_db/repository/admin_repository.py
@@ -4,6 +4,7 @@ from typing import Callable
 from bridgecard_core_sdk.core.core_auth.core_auth import CoreAuth
 from ..core_db import DbSession
 from .base_repository import BaseRepository
+from typing import Any, Callable, Optional
 
 
 ADMINISTRATORS_MODEL_NAME = "administrators"
@@ -19,3 +20,21 @@ class AdminRepository(BaseRepository):
         self.auth = CoreAuth(
             admin_repo=self,
         )
+
+
+    def update_admin_attr(
+        self,
+        field: str,
+        id: str,
+        value,
+        context: Optional[Any] = None,
+    ):
+        try:
+
+            self.db_ref.child(id).child(field).update(value)
+                
+            return True
+
+        except:
+            return None
+    
