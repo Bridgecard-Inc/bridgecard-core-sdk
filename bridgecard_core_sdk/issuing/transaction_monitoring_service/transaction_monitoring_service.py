5a70c91545e55efefa600a200b63deab27f4ea18
Ife-the-lion <olorunfemi_i@yahoo.com>
2024-05-09 12:11:12 +0200

    transaction monitoring

--- bridgecard_core_sdk/issuing/transaction_monitoring_service/transaction_monitoring_service.py
+++ bridgecard_core_sdk/issuing/transaction_monitoring_service/transaction_monitoring_service.py
@@ -32,7 +32,7 @@ def init_transaction_monitoring_service():
     server_addr = (
         "a08c5b25f952f4266b52c70600aefcec-2060662335.us-west-2.elb.amazonaws.com:80"
     )
-    server_addr = "localhost:8085"
+    # server_addr = "localhost:8085"
 
     # creds = grpc.ssl_channel_credentials(
     #     private_key=client_private_key.encode(),
