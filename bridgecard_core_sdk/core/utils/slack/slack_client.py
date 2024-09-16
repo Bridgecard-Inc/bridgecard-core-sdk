4e994b367aa5f70d7551a215a5d2145931813466
IO <olorunfemi_i@yahoo.com>
2024-09-16 09:55:40 +0200

    slack delete message

--- bridgecard_core_sdk/core/utils/slack/slack_client.py
+++ bridgecard_core_sdk/core/utils/slack/slack_client.py
@@ -28,24 +28,35 @@ class SlackClient:
         return ts
 
     def upload_file(
-            self,
-            channels: Union[str, list],
-            file: Union[str, IO],
-            filename: Optional[str] = None,
-            initial_comment: Optional[str] = None,
-            thread_ts: Optional[str] = None,
-        ):
-            """
-            Upload a file to Slack.
-            :param channels: Channel ID(s) to share the file to (string or list of strings)
-            :param file: File content or path to the file
-            :param filename: Filename of the file (optional)
-            :param initial_comment: Initial comment to add to the file (optional)
-            :param thread_ts: Thread timestamp to upload the file to (optional)
-            :return: Response from Slack API
-            """
-            if isinstance(channels, list):
-                channels = ",".join(channels)
+        self,
+        channels: Union[str, list],
+        file: Union[str, IO],
+        filename: Optional[str] = None,
+        initial_comment: Optional[str] = None,
+        thread_ts: Optional[str] = None,
+    ):
+        """
+        Upload a file to Slack.
+        :param channels: Channel ID(s) to share the file to (string or list of strings)
+        :param file: File content or path to the file
+        :param filename: Filename of the file (optional)
+        :param initial_comment: Initial comment to add to the file (optional)
+        :param thread_ts: Thread timestamp to upload the file to (optional)
+        :return: Response from Slack API
+        """
+        if isinstance(channels, list):
+            channels = ",".join(channels)
+        # Ensure file is opened if it's a path
+        if isinstance(file, str):
+            with open(file, 'rb') as f:
+                res = self._client.files_upload_v2(
+                    channels=channels,
+                    file=f,
+                    filename=filename,
+                    initial_comment=initial_comment,
+                    thread_ts=thread_ts
+                )
+        else:
             res = self._client.files_upload_v2(
                 channels=channels,
                 file=file,
@@ -53,4 +64,18 @@ class SlackClient:
                 initial_comment=initial_comment,
                 thread_ts=thread_ts
             )
-            return res
+        return res
+    
+    def delete_message(self, channel: str, ts: str):
+       """
+       Delete a message from Slack.
+       :param channel: Channel ID where the message is located
+       :param ts: Timestamp of the message to delete
+       :return: Response from Slack API
+       """
+       try:
+           response = self._client.chat_delete(channel=channel, ts=ts)
+           return response
+       except slack_sdk.errors.SlackApiError as e:
+           print(f"Error deleting message: {e.response['error']}")
+           return None
